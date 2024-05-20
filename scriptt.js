fe913bd444bbe875f577e75c99700f216d29f36d
athenapay <164018731+athenapay@users.noreply.github.com>
2024-05-20 15:52:23 +0800

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,17 +1,17 @@
 let allVideos = [
    {
       name: "New day, New play",
-      src: "Ep 1 [E014085]",
+      src: "Ep 1 [E014085] (1)",
       id: "vid_1"
    },
    {
       name: "Team Slay or Fail",
-      src: "Ep 2 na [A8D1A98]",
+      src: "Ep 2 na [A8D1A98] (1)",
       id: "vid_2"
    },
    {
       name: "Mayday! Mayday!",
-      src: "Ep 3 [CA4DA45]",
+      src: "Ep 3 [CA4DA45] (1)",
       id: "vid_3"
    },
    
