88cfa858f982e3fbab2e2580341a4888dbe288b9
athenapay <164018731+athenapay@users.noreply.github.com>
2024-05-20 15:50:57 +0800

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,18 @@
+let allVideos = [
+   {
+      name: "New day, New play",
+      src: "Ep 1 [E014085]",
+      id: "vid_1"
+   },
+   {
+      name: "Team Slay or Fail",
+      src: "Ep 2 na [A8D1A98]",
+      id: "vid_2"
+   },
+   {
+      name: "Mayday! Mayday!",
+      src: "Ep 3 [CA4DA45]",
+      id: "vid_3"
+   },
+   
+]
